5e50d8d87ecc2b70bf08ea7d4ecc5156d2d07255
rarayukawa <r1u2u18@gmail.com>
2021-09-13 14:24:06 +0900

    var

---
+++ Varval.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var counterVar: Int = 0
+    println(counterVar)
+    counterVar++
+    println(counterVar)
+
+    var versionVar: Double = 1.0
+    println(versionVar)
+    versionVar = 2.0
+    println(versionVar)
+}
