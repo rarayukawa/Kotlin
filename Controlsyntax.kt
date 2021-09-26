2a6cae85bbcc162976f946bd04db98921c48f844
rarayukawa <r1u2u18@gmail.com>
2021-09-26 17:08:57 +0900

    if

---
+++ Controlsyntax.kt
@@ -0,0 +1,18 @@
+fun main() {
+    if(true) {
+        //true
+    }
+
+    if(false) {
+        //false
+    }
+
+    val i = if (true) {
+        1
+    } else {
+        100
+    }
+
+    println(i)
+   
+}
