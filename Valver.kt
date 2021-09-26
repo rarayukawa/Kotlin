b97f68825e570b7eb53cf008f61f4c883e0d20b8
rarayukawa <r1u2u18@gmail.com>
2021-09-26 16:25:44 +0900

    型推論

---
+++ Valver.kt
@@ -0,0 +1,11 @@
+class Valver {
+    val number = 0
+    val string = ""
+    var double = 12.514
+    val list = listOf(1, 2)
+    val set = setOf(1, 2)
+    val map = mapOf("a" to 10)
+
+    fun echo(messoge: String) = println("message: $message")
+    fun joinString(message: String) = "message: $message"
+}
